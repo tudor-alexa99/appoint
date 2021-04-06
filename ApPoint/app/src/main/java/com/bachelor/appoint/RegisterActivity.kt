7aed61e3eceaff44f8c79c27d6c119fb6728461b
Tudor Alexa <tudor.alexa99@gmail.com>
2021-04-07 00:51:19 +0300

    Create Register Activity

--- ApPoint/app/src/main/java/com/bachelor/appoint/RegisterActivity.kt
+++ ApPoint/app/src/main/java/com/bachelor/appoint/RegisterActivity.kt
@@ -3,10 +3,9 @@ package com.bachelor.appoint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register   )
-        setSupportActionBar(findViewById(R.id.custom_toolbar))
+        setContentView(R.layout.activity_register)
     }
 }
