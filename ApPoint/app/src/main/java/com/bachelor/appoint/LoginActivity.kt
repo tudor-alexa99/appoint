9d03c43bce9207f4fa50092c83ce4ba07b9fad7a
Tudor Alexa <tudor.alexa99@gmail.com>
2021-04-12 10:24:43 +0300

    Fix layout inflater

--- ApPoint/app/src/main/java/com/bachelor/appoint/LoginActivity.kt
+++ ApPoint/app/src/main/java/com/bachelor/appoint/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.bachelor.appoint
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.TextView
+import com.bachelor.appoint.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -20,5 +26,17 @@ class LoginActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+
+//        This is the new ViewBinding method
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.tvGoToRegister.setOnClickListener {
+            Log.d("onClickListener", "tv_Register clicked")
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
+
+
+
     }
 }
--- ApPoint/app/src/main/java/com/bachelor/appoint/RegisterActivity.kt
+++ ApPoint/app/src/main/java/com/bachelor/appoint/RegisterActivity.kt
@@ -1,11 +1,44 @@
 package com.bachelor.appoint
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.bachelor.appoint.databinding.ActivityLoginBinding
+import com.bachelor.appoint.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        Log.d("Create bind", binding.toString())
+        val view = binding.root
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+
+        binding.tvGoToLogin.setOnClickListener {
+            Log.d("onClickListener", "tv_Login clicked")
+            intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener{
+            Log.d("onCreate", "Button Clicked")
+        }
+
+        setContentView(view)
     }
 }
