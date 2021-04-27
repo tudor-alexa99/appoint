ae755313bf430f8df40c12160c651095257b275d
Tudor Alexa <tudor.alexa99@gmail.com>
2021-04-27 13:59:02 +0300

    Add business card

--- ApPoint/app/src/main/java/com/bachelor/appoint/ui/MyAppointmensAdapter.kt
+++ ApPoint/app/src/main/java/com/bachelor/appoint/ui/MyAppointmensAdapter.kt
@@ -11,7 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.bachelor.appoint.R
 import com.bachelor.appoint.data.appointmentsList
-import com.bachelor.appoint.databinding.AppointmentsRowBinding
+import com.bachelor.appoint.databinding.CardAppointmentBinding
 import com.bachelor.appoint.model.Appointment
 import com.bachelor.appoint.viewModel.AppointmentsViewModel
 import org.w3c.dom.Text
@@ -21,7 +21,7 @@ class MyAppointmensAdapter(
     private var appointments: MutableList<Appointment>,
 
 ): RecyclerView.Adapter<MyAppointmensAdapter.AppointmentsViewHolder>() {
-    class AppointmentsViewHolder(val binding: AppointmentsRowBinding): RecyclerView.ViewHolder(binding.root) {
+    class AppointmentsViewHolder(val binding: CardAppointmentBinding): RecyclerView.ViewHolder(binding.root) {
         val tv_place: TextView = binding.root.findViewById(R.id.tv_place)
         val tv_startTime: TextView = binding.root.findViewById(R.id.tv_startTime)
 
@@ -33,11 +33,11 @@ class MyAppointmensAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppointmentsViewHolder {
-        return AppointmentsViewHolder(AppointmentsRowBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return AppointmentsViewHolder(CardAppointmentBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: AppointmentsViewHolder, position: Int) {
-        holder.binding.appoitmentCard.setOnClickListener{Log.d("tag", "clicked")}
+        holder.binding.appointmentCard.setOnClickListener{Log.d("tag", "clicked")}
         holder.bindView(appointments[position])
     }
 
