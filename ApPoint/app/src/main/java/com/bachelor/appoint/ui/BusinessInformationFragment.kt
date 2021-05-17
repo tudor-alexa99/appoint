d5031ca00c29c0ca6302ba92c4b16859127ff373
Tudor Alexa <tudor.alexa99@gmail.com>
2021-05-17 14:00:53 +0300

    Finish business details fragment

--- ApPoint/app/src/main/java/com/bachelor/appoint/ui/BusinessInformationFragment.kt
+++ ApPoint/app/src/main/java/com/bachelor/appoint/ui/BusinessInformationFragment.kt
@@ -1,7 +1,7 @@
 package com.bachelor.appoint.ui
 
+import kotlin.math.round
 import android.animation.ObjectAnimator
-import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,10 +9,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bachelor.appoint.R
-import com.bachelor.appoint.databinding.CardPlaceBinding
 import com.bachelor.appoint.databinding.FragmentBusinessInformatioBinding
 import com.bachelor.appoint.model.Business
 import com.bachelor.appoint.utils.Constants
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -71,13 +72,35 @@ class BusinessInformatioFragment : Fragment() {
         // set the name
         binding.tvCompanyName.text = currentBusiness.name
 
-        // set the risk progressBar
+        // set the risk progressBar and animation
         val objectAnimator = ObjectAnimator()
         business?.let { //binding.statsProgressbar.setProgress(it.estimatedRisk, true)
             ObjectAnimator.ofInt(binding.statsProgressbar, "progress", it.estimatedRisk)
                 .setDuration(900)
                 .start()
-         }
+        }
+
+        // set the number inside the pie chart
+        binding.tvScore.text = business!!.estimatedRisk.toString()
+
+        // set the business type
+        binding.tvBiTypeVal.text = business!!.type
+
+        // set the seats number
+        binding.tvBiSeatsVal.text = business!!.seatsNumber.toString()
+
+        // compute and round the average area for each user
+        binding.tvBiUserAreaVal.text =
+            ("%.1f".format((business!!.estimatedSurface.toFloat() / business!!.seatsNumber))) + " m²"
+
+        // format and set the average time
+        val time = LocalTime.parse(business!!.averageTimeSpent, DateTimeFormatter.ofPattern("H:mm"))
+        val timeValue = time.hour.toString() + "hr " + time.minute.toString() + "min"
+        binding.tvBiAvgTimeSpentVal.text = timeValue
+
+        // set the address
+        binding.tvBiAddressVal.text = business!!.location
+
     }
 
     companion object {
