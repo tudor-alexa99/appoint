45d6c5055bb492efd587cf39514b2a4b24a04364
Tudor Alexa <tudor.alexa99@gmail.com>
2021-05-26 19:47:26 +0300

    Add chart and design buttons

--- ApPoint/app/src/main/java/com/bachelor/appoint/ReportActivity.kt
+++ ApPoint/app/src/main/java/com/bachelor/appoint/ReportActivity.kt
@@ -2,10 +2,45 @@ package com.bachelor.appoint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bachelor.appoint.databinding.ActivityReportBinding
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAChart
 
 class ReportActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityReportBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report)
+
+        binding = ActivityReportBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var aaChartView = binding.aaChartView
+
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Areaspline)
+            .subtitle("Risk value for upcoming events")
+            .backgroundColor("#4D000000")
+            .dataLabelsEnabled(false)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Completed events")
+                    .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5))
+                    .reversed(true),
+                AASeriesElement()
+                    .name("Upcoming events")
+                    .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5))
+                    .color("#85C7B5"),
+//                AASeriesElement()
+//                    .name("London")
+//                    .data(arrayOf(0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0)),
+//                AASeriesElement()
+//                    .name("Berlin")
+//                    .data(arrayOf(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8))
+                )
+            )
+        aaChartView.aa_drawChartWithChartModel(aaChartModel)
     }
 }
